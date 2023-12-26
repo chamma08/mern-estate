c9ee4a8180f3bd575ea3baed9793168f25f1a27a
Matheesha Dissanayake <SA22524904@my.sliit.lk>
2023-12-26 09:37:48 +0530

    Connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
 import  express  from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+//line 2 and 3 to hide password from others
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('Connected to MongoDB!');
+    }).catch((err) => {
+    console.log(err);
+    })
 
 const app = express();
 
