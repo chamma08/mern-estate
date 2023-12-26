0422c26d5906a9a313c1f40e211a470c6f4ec09f
Matheesha Dissanayake <SA22524904@my.sliit.lk>
2023-12-26 08:58:03 +0530

    create header components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import SignUp from './pages/SignUp';
 import About from './pages/About';
 import Profile from './pages/Profile';
+import Header from './components/Header';
 
 export default function App() {
 
